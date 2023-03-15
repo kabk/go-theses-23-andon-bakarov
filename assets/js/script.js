cf68bf5070bc889febe993ad7cc98168bea229fc
BojaKrava <andonbakarov13@gmail.com>
2023-03-15 15:32:05 +0200

    typeface, menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-// const scrollContainer = document.querySelector("main");
+const scrollContainer = document.querySelector("main");
 
-// scrollContainer.addEventListener("wheel", (evt) => {
-//     evt.preventDefault();
-//     scrollContainer.scrollLeft += evt.deltaY;
-// });
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+});
